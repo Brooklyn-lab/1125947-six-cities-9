6b813f1622f978da468d983dca7302894d26f014
Brooklyn-lab <stepanyukilja@gmail.com>
2022-02-17 18:57:05 +0200

    add new components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,8 @@
+// import MainScreen from '../main-screen/main-screen';
+import MainEmptyScreen from '../main-empty-screen/main-empty-screen';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return <MainEmptyScreen />;
 }
 
 export default App;
