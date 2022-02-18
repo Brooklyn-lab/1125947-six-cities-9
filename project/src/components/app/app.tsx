984e3c1f2142798ab8e42e69bbe469cf61d6d237
Brooklyn-lab <stepanyukilja@gmail.com>
2022-02-18 10:39:06 +0200

    add component login

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,17 @@
-import MainScreen from '../main-screen/main-screen';
+// import MainScreen from '../main-screen/main-screen';
+
+import LoginScreen from '../login-screen/login-screen';
+
+// type PlaceCardProps = {
+//   placesCount: number,
+// }
+
+// function App({placesCount}: PlaceCardProps): JSX.Element {
+//   return <MainScreen placesCount={placesCount} />;
+// }
 
 function App(): JSX.Element {
-  return <MainScreen />;
+  return <LoginScreen />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+// const CardInfo = {
+//   placesCount: 312,
+// };
+
 ReactDOM.render(
   <React.StrictMode>
-    <App priceValue: ='60' text='' />
+    {/* <App placesCount = {CardInfo.placesCount} /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root'));
